f08149293723229ec49a61271af9ab50127c30f6
Dragan Pepic <nedimco2006@gmail.com>
2022-01-14 17:57:30 +0100

    Add project files.

---
+++ Server/Haab.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRPrimer.Server
+{
+    public class Haab : Hub
+    {
+        public async Task Test(int x, int y)
+        {
+            Console.WriteLine($"{x} {y}");
+
+            await Clients.Caller.SendAsync("EvoTiZbir", x + y, "zdravooo", x - y);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using SignalRPrimer.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapHub<Haab>("test");
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
